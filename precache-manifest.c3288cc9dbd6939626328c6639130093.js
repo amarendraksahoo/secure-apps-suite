4a4d5de2e6d0d04f1d1efcd83447ac256d5a37d5
Arun Kumar Selvaraj <dev@ioak.org>
2019-07-06 22:17:36 +0530

    Updates

---
+++ precache-manifest.c3288cc9dbd6939626328c6639130093.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a2ac836b9a941257fa64b6b1571a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02103c1d6aefac942786",
+    "url": "/static/css/main.e1295787.chunk.css"
+  },
+  {
+    "revision": "142ea7e3136929002433",
+    "url": "/static/js/2.cb0b37e4.chunk.js"
+  },
+  {
+    "revision": "02103c1d6aefac942786",
+    "url": "/static/js/main.6962b277.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
